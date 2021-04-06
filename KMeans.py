d3c076c6cb7a3347bca5ce34575e704e44ad54df
alexzamurca <alexzamurca@yahoo.co.uk>
2021-04-06 18:03:37 +0100

    renamed classes in KMeans.py to indicate them as private

--- KMeans.py
+++ KMeans.py
@@ -28,16 +28,16 @@ class KMeans:
         self.cluster_centers_ = np.zeros(shape=(self.n_clusters, len(data[0])), dtype=float)
         self.labels_ = np.zeros(shape=(len(data)))
         # "Create a random centroid for each cluster."
-        self.determine_cluster_centers(data)
+        self.__determine_random_cluster_centers(data)
 
         # Run until the arrays are equivalent
         while True:
             # Initialise the variables needed per loop
-            self.init_variables(data)
+            self.__init_variables(data)
             # "For each data point identify the closest centroid and assign it to the corresponding cluster."
-            self.label_data(data)
+            self.__label_data(data)
             # "Compute a new centroid for each cluster based on the current cluster members"
-            self.update_centroids()
+            self.__update_centroids()
 
             # Check if the centroids from the previous iteration is equivalent to the current iteration (means we can stop iterating)
             arrays_equivalent = np.array_equal(self.old_cluster_centers, self.cluster_centers_)
@@ -47,13 +47,13 @@ class KMeans:
             else:
                 self.old_cluster_centers = np.copy(self.cluster_centers_)
 
-    def init_variables(self, data: np.array) -> None:
+    def __init_variables(self, data: np.array) -> None:
         # Here we set the points in cluster to an empty array according to the data
         self.points_in_cluster = []
         for i in range(self.n_clusters):
             self.points_in_cluster.append(np.zeros((0, len(data[0])), dtype=int))
 
-    def determine_cluster_centers(self, data: np.array) -> None:
+    def __determine_random_cluster_centers(self, data: np.array) -> None:
         # Randomise n different points
         for i in range(self.n_clusters):
             # Randomise an index for the center point
@@ -61,17 +61,17 @@ class KMeans:
             # Get data point at that range and set it to a centroid
             self.cluster_centers_[i] = data[index]
 
-    def label_data(self, data: np.array) -> None:
+    def __label_data(self, data: np.array) -> None:
         # Go through each data point
         for i in range(len(data)):
             # Get the closest cluster to the current point
-            index_of_closest_cluster = self.get_closest_cluster(data[i])
+            index_of_closest_cluster = self.__get_closest_cluster(data[i])
             # Add to labels array
             self.labels_[i] = index_of_closest_cluster
             # Add the point to the cluster specific array (used later for computing the new centroids)
             self.points_in_cluster[index_of_closest_cluster] = np.append(self.points_in_cluster[index_of_closest_cluster], [data[i]], axis=0)
 
-    def get_closest_cluster(self, data_point: np.array) -> int:
+    def __get_closest_cluster(self, data_point: np.array) -> int:
         # index of closest cluster
         i = 0
         # smallest distance
@@ -89,7 +89,7 @@ class KMeans:
         # after looping through all cluster centers the one that has the smallest distance at the end is the closest so return
         return i
 
-    def update_centroids(self) -> None:
+    def __update_centroids(self) -> None:
         # loop through the clusters centers
         for i in range(len(self.cluster_centers_)):
             # find the sum of columns of the data points
